3a656b003a6f10826e5a57ec369baa45eacfe875
Fluffyalien1422 <shawnbhumbla2@gmail.com>
2023-02-25 19:59:20 -0800

    fix copying incorrect dir to com.mojang

--- src/index.ts
+++ src/index.ts
@@ -30,203 +30,216 @@ import { createWriteStream, existsSync } from "fs";
 import archiver from "archiver";
 
 const FilterDefinitionOptions = z.object({
-  arguments: z.any().optional()
+	arguments: z.any().optional()
 });
 
 type TFilterDefinitionOptions = z.infer<typeof FilterDefinitionOptions>;
 
 interface IFilterData {
-  args: {
-    [arg: string]: unknown;
-  };
+	args: {
+		[arg: string]: unknown;
+	};
 }
 
 class Filter {
-  mainFunction;
-  definitionOptions;
+	mainFunction;
+	definitionOptions;
 
-  constructor(mainFunction: (data: IFilterData) => void, definitionOptions: TFilterDefinitionOptions) {
-    this.mainFunction = mainFunction;
-    this.definitionOptions = definitionOptions;
-  }
+	constructor(mainFunction: (data: IFilterData) => void, definitionOptions: TFilterDefinitionOptions) {
+		this.mainFunction = mainFunction;
+		this.definitionOptions = definitionOptions;
+	}
 }
 
 interface IFilters {
-  [id: string]: Filter;
+	[id: string]: Filter;
 }
 
 async function evalFilters(config: types.IConfigEvaluated, buildFlags: any) {
-  const context = {
-    buildFlags,
-    config
-  };
+	const context = {
+		buildFlags,
+		config
+	};
 
-  const api = await apiExtensions.evalExtensions(context);
+	const api = await apiExtensions.evalExtensions(context);
 
-  const filters: IFilters = {};
+	const filters: IFilters = {};
 
-  async function evalFilter(filePath: string, namespace: string, id: string) {
-    let definitionOptions: TFilterDefinitionOptions | undefined;
+	async function evalFilter(filePath: string, namespace: string, id: string) {
+		let definitionOptions: TFilterDefinitionOptions | undefined;
 
-    const vmContext = vm.createContext({
-      filter: (o: TFilterDefinitionOptions = {}) => (definitionOptions = o),
-      main() {
-        throw "Main function not defined.";
-      },
-      context: {
-        namespace,
-        ...context
-      },
-      api: {
-        $this: api[namespace] ?? {},
-        ...api
-      }
-    });
+		const vmContext = vm.createContext({
+			filter: (o: TFilterDefinitionOptions = {}) => (definitionOptions = o),
+			main() {
+				throw "Main function not defined.";
+			},
+			context: {
+				namespace,
+				...context
+			},
+			api: {
+				$this: api[namespace] ?? {},
+				...api
+			}
+		});
 
-    vm.runInContext(await fs.readFile(filePath, "utf8"), vmContext);
+		vm.runInContext(await fs.readFile(filePath, "utf8"), vmContext);
 
-    filters[id] = new Filter(
-      vmContext.main,
-      errors.ZodError.tryParseSchema(
-        FilterDefinitionOptions,
-        definitionOptions,
-        errors.ErrorCode.NoErrorCode,
-        errors.RuntimeError(errors.ErrorCode.RuntimeEvalFilterParseDefOpts, id, "Invalid definition options.")
-      )
-    );
-  }
+		filters[id] = new Filter(
+			vmContext.main,
+			errors.ZodError.tryParseSchema(
+				FilterDefinitionOptions,
+				definitionOptions,
+				errors.ErrorCode.NoErrorCode,
+				errors.RuntimeError(errors.ErrorCode.RuntimeEvalFilterParseDefOpts, id, "Invalid definition options.")
+			)
+		);
+	}
 
-  const promises: Promise<void>[] = [];
+	const promises: Promise<void>[] = [];
 
-  function awaitFilterPromise(id: string, promise: Promise<void>) {
-    promises.push(
-      promise.catch((e) => {
-        throw errors.RuntimeError(errors.ErrorCode.RuntimeEvalFilterAwaitFilterProm, id, e);
-      })
-    );
-  }
+	function awaitFilterPromise(id: string, promise: Promise<void>) {
+		promises.push(
+			promise.catch(e => {
+				throw errors.RuntimeError(errors.ErrorCode.RuntimeEvalFilterAwaitFilterProm, id, e);
+			})
+		);
+	}
 
-  for (const filePath of glob.sync(`${config.srcPath}/filters/**/*.js`, {
-    nodir: true
-  })) {
-    const id = path.basename(filePath, ".js");
-    awaitFilterPromise(id, evalFilter(filePath, "project", id));
-  }
+	for (const filePath of glob.sync(`${config.srcPath}/filters/**/*.js`, {
+		nodir: true
+	})) {
+		const id = path.basename(filePath, ".js");
+		awaitFilterPromise(id, evalFilter(filePath, "project", id));
+	}
 
-  for (const filePath of glob.sync(`.blockbuild/modules/*/filters/**/*.js`, {
-    nodir: true
-  })) {
-    const namespace = filePath.split("/")[2];
-    const id = `${namespace}:${path.basename(filePath, ".js")}`;
-    awaitFilterPromise(id, evalFilter(filePath, namespace, id));
-  }
+	for (const filePath of glob.sync(`.blockbuild/modules/*/filters/**/*.js`, {
+		nodir: true
+	})) {
+		const namespace = filePath.split("/")[2];
+		const id = `${namespace}:${path.basename(filePath, ".js")}`;
+		awaitFilterPromise(id, evalFilter(filePath, namespace, id));
+	}
 
-  await Promise.all(promises);
+	await Promise.all(promises);
 
-  return filters;
+	return filters;
 }
 
 export async function build(config: types.IConfigEvaluated, buildFlags: any) {
-  const packPath = path.join(config.srcPath, "packs");
-  const BPPath = path.join(packPath, "BP");
-  const RPPath = path.join(packPath, "RP");
+	const packPath = path.join(config.srcPath, "packs");
+	const BPPath = path.join(packPath, "BP");
+	const RPPath = path.join(packPath, "RP");
 
-  if (config.packs.includes("BP") && !existsSync(BPPath))
-    throw errors.InternalError(
-      errors.ErrorCode.InternalBPNotFound,
-      `Missing behavior pack directory at \`${BPPath}\`. If you want to choose a different location, change the \`srcPath\` value in the config. If you do not want a behavior pack, remove \`BP\` from \`packs\` in the config.`
-    );
-  if (config.packs.includes("RP") && !existsSync(RPPath))
-    throw errors.InternalError(
-      errors.ErrorCode.InternalRPNotFound,
-      `Missing resource pack directory at \`${RPPath}\`. If you want to choose a different location, change the \`srcPath\` value in the config. If you do not want a resource pack, remove \`RP\` from \`packs\` in the config.`
-    );
+	if (config.packs.includes("BP") && !existsSync(BPPath))
+		throw errors.InternalError(
+			errors.ErrorCode.InternalBPNotFound,
+			`Missing behavior pack directory at \`${BPPath}\`. If you want to choose a different location, change the \`srcPath\` value in the config. If you do not want a behavior pack, remove \`BP\` from \`packs\` in the config.`
+		);
+	if (config.packs.includes("RP") && !existsSync(RPPath))
+		throw errors.InternalError(
+			errors.ErrorCode.InternalRPNotFound,
+			`Missing resource pack directory at \`${RPPath}\`. If you want to choose a different location, change the \`srcPath\` value in the config. If you do not want a resource pack, remove \`RP\` from \`packs\` in the config.`
+		);
 
-  let filters: unknown;
+	let filters: unknown;
 
-  await fs.rm(config.outPath, { force: true, recursive: true });
+	await fs.rm(config.outPath, { force: true, recursive: true });
 
-  await Promise.all([evalFilters(config, buildFlags).then((res) => (filters = res)), fs.cp(packPath, config.outPath, { recursive: true })]);
+	await Promise.all([
+		evalFilters(config, buildFlags).then(res => (filters = res)),
+		fs.cp(packPath, config.outPath, { recursive: true })
+	]);
 
-  for (const filterToExecute of config.filters) {
-    const filter = (filters as IFilters)[filterToExecute.id];
-    if (!filter)
-      throw errors.InternalError(
-        errors.ErrorCode.InternalBuildFilterNotFound,
-        `Cannot execute filter with id \`${filterToExecute.id}\` because it does not exist.`
-      );
+	for (const filterToExecute of config.filters) {
+		const filter = (filters as IFilters)[filterToExecute.id];
+		if (!filter)
+			throw errors.InternalError(
+				errors.ErrorCode.InternalBuildFilterNotFound,
+				`Cannot execute filter with id \`${filterToExecute.id}\` because it does not exist.`
+			);
 
-    let args;
+		let args;
 
-    if (filter.definitionOptions.arguments) {
-      if (!filter.definitionOptions.arguments.parse)
-        throw errors.RuntimeError(
-          errors.ErrorCode.RuntimeArgumentsSchemaNoParseMethod,
-          filterToExecute.id,
-          "Arguments schema provided in the definition options does not have a `parse` method."
-        );
-      try {
-        args = await filter.definitionOptions.arguments.parse(filterToExecute.arguments);
-      } catch (err) {
-        throw errors.RuntimeError(
-          errors.ErrorCode.RuntimeArgumentsSchemaParseMethodThrew,
-          filterToExecute.id,
-          `\`arguments.parse\` (from definition options) threw an error or could not be called.\n\t${err}`
-        );
-      }
-    }
+		if (filter.definitionOptions.arguments) {
+			if (!filter.definitionOptions.arguments.parse)
+				throw errors.RuntimeError(
+					errors.ErrorCode.RuntimeArgumentsSchemaNoParseMethod,
+					filterToExecute.id,
+					"Arguments schema provided in the definition options does not have a `parse` method."
+				);
+			try {
+				args = await filter.definitionOptions.arguments.parse(filterToExecute.arguments);
+			} catch (err) {
+				throw errors.RuntimeError(
+					errors.ErrorCode.RuntimeArgumentsSchemaParseMethodThrew,
+					filterToExecute.id,
+					`\`arguments.parse\` (from definition options) threw an error or could not be called.\n\t${err}`
+				);
+			}
+		}
 
-    try {
-      await filter.mainFunction({
-        args
-      });
-    } catch (err) {
-      throw errors.RuntimeError(errors.ErrorCode.RuntimeMainFunctionThrew, filterToExecute.id, `Main function threw an error.\n\t${err}`);
-    }
-  }
+		try {
+			await filter.mainFunction({
+				args
+			});
+		} catch (err) {
+			throw errors.RuntimeError(
+				errors.ErrorCode.RuntimeMainFunctionThrew,
+				filterToExecute.id,
+				`Main function threw an error.\n\t${err}`
+			);
+		}
+	}
 
-  if (!buildFlags.production && !buildFlags.package) {
-    // copy packs to com.mojang
-    if (config.packs.includes("BP"))
-      (async () => {
-        const destPath = path.join(config.comMojangPath, "development_behavior_packs", config.packName);
-        await fs.rm(destPath, { recursive: true, force: true });
-        await fs.cp(BPPath, destPath, { recursive: true });
-        console.log(`Copied \`${BPPath}\` to \`${destPath}\`. Use \`--production\` to disable this.`);
-      })();
+	if (!buildFlags.production && !buildFlags.package) {
+		const outBPPath = path.join(config.outPath, "BP");
+		const outRPPath = path.join(config.outPath, "RP");
 
-    if (config.packs.includes("RP"))
-      (async () => {
-        const destPath = path.join(config.comMojangPath, "development_resource_packs", config.packName);
-        await fs.rm(destPath, { recursive: true, force: true });
-        await fs.cp(RPPath, destPath, { recursive: true });
-        console.log(`Copied \`${RPPath}\` to \`${destPath}\`. Use \`--production\` to disable this.`);
-      })();
-  }
+		// copy packs to com.mojang
+		if (config.packs.includes("BP"))
+			(async () => {
+				const destPath = path.join(config.comMojangPath, "development_behavior_packs", config.packName);
+				await fs.rm(destPath, { recursive: true, force: true });
+				await fs.cp(outBPPath, destPath, { recursive: true });
+				console.log(`Copied \`${outBPPath}\` to \`${destPath}\`. Use \`--production\` to disable this.`);
+			})();
 
-  if (buildFlags.package) {
-    // zip dist to .mcaddon
-    if (existsSync("tmp"))
-      throw errors.InternalError(errors.ErrorCode.InternalBuildPackageTmpFileExists, `Cannot package project. There is already a file at \`tmp\`.`);
+		if (config.packs.includes("RP"))
+			(async () => {
+				const destPath = path.join(config.comMojangPath, "development_resource_packs", config.packName);
+				await fs.rm(destPath, { recursive: true, force: true });
+				await fs.cp(outRPPath, destPath, { recursive: true });
+				console.log(`Copied \`${outRPPath}\` to \`${destPath}\`. Use \`--production\` to disable this.`);
+			})();
+	}
 
-    const output = createWriteStream("tmp");
-    const archive = archiver("zip");
+	if (buildFlags.package) {
+		// zip dist to .mcaddon
+		if (existsSync("tmp"))
+			throw errors.InternalError(
+				errors.ErrorCode.InternalBuildPackageTmpFileExists,
+				`Cannot package project. There is already a file at \`tmp\`.`
+			);
 
-    archive.on("warning", (err) => {
-      throw errors.InternalError(errors.ErrorCode.InternalBuildPackageArchiverWarning, err);
-    });
+		const output = createWriteStream("tmp");
+		const archive = archiver("zip");
 
-    archive.on("error", (err) => {
-      throw errors.InternalError(errors.ErrorCode.InternalBuildPackageArchiverError, err);
-    });
+		archive.on("warning", err => {
+			throw errors.InternalError(errors.ErrorCode.InternalBuildPackageArchiverWarning, err);
+		});
 
-    archive.pipe(output);
-    archive.directory(config.outPath, false);
+		archive.on("error", err => {
+			throw errors.InternalError(errors.ErrorCode.InternalBuildPackageArchiverError, err);
+		});
 
-    await archive.finalize();
+		archive.pipe(output);
+		archive.directory(config.outPath, false);
 
-    const packagedArchivePath = path.join(config.outPath, `${config.packName}.mcaddon`);
-    await fs.rename("tmp", packagedArchivePath);
-    console.log(`Packaged files at \`${config.outPath}\` into archive \`${packagedArchivePath}\`.`);
-  }
+		await archive.finalize();
+
+		const packagedArchivePath = path.join(config.outPath, `${config.packName}.mcaddon`);
+		await fs.rename("tmp", packagedArchivePath);
+		console.log(`Packaged files at \`${config.outPath}\` into archive \`${packagedArchivePath}\`.`);
+	}
 }
